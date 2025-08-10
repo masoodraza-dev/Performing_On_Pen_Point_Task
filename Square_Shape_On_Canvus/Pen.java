a34d5e41a2a17dee3407127186e7bb11c1e4a942
masoodraza-dev <masoodr508@gmail.com>
2025-08-11 01:22:24 +0500

    Create Pen.java
    Pen class

---
+++ Square_Shape_On_Canvus/Pen.java
@@ -0,0 +1,119 @@
+import java.util.*;
+
+public class Pen {
+    private Point location;
+    private ArrayList<String> pathHistory;
+    private ArrayList<Point> path;
+
+    public Pen() {
+        location = new Point(0, 0);
+        pathHistory = new ArrayList<>();
+        path = new ArrayList<>();
+        pathHistory.add("Pen movements started at: " + location);
+        // path.add(location.copy());
+    }
+
+    public void MoveTo(Point newLocation) {
+        location = newLocation;
+        System.out.println("Pen moved to " + location);
+        pathHistory.add("Pen moved to " + location);
+        path.add(location.copy());
+
+    }
+
+    public void LineTo(Point newLocation) {
+        pathHistory.add("Line drawn from " + location + " To " + newLocation);
+        path.add(location.copy());
+        location = newLocation;
+        path.add(location.copy());
+        System.out.println("Draw line from " + location + " To " + newLocation);
+
+    }
+
+    public void showHistory() {
+        System.out.println("<==== HISTORY OF PEN MOVEMENTS ====>");
+        for (String moves : pathHistory) {
+            System.out.println(moves);
+        }
+    }
+
+    // method to draw square qo-ordinates
+    public void drawSquare(int lineLength) {
+        Point start = location.copy();
+
+        // first Start moves towards Right (x-axis) --> of line length 5, why 5 ?
+        // because we are passing 5 in arguments in Main.java class
+        Point p1 = new Point(location.getx() + lineLength, location.gety());
+        LineTo(p1);
+
+        Point p2 = new Point(p1.getx(), p1.gety() + lineLength);
+        LineTo(p2);
+
+        Point p3 = new Point(p2.getx() - lineLength, p2.gety());
+        LineTo(p3);
+
+        LineTo(start);
+    }
+
+    // this method is for rectangle and designed as like width = 2*height
+    public void drawRectangle(int lineLength) {
+        Point start = location;
+        System.out.println("Rectangle through only one attribute:");
+
+        Point p1 = new Point(location.getx() + lineLength + lineLength, location.gety());
+        LineTo(p1);
+
+        Point p2 = new Point(p1.getx(), p1.gety() + lineLength);
+        LineTo(p2);
+
+        Point p3 = new Point(p2.getx() - lineLength - lineLength, p2.gety());
+        LineTo(p3);
+
+        LineTo(start);
+
+    }
+
+    // Making this rectangle through giving two attributes are width and height
+    public void drawRectangle2(int width, int height) {
+        Point start = location;
+
+        System.out.println("Making this is rectangle 2");
+
+        Point p1 = new Point(location.getx() + width, location.gety());
+        LineTo(p1);
+
+        Point p2 = new Point(p1.getx(), p1.gety() + height);
+        LineTo(p2);
+
+        Point p3 = new Point(p2.getx() - width, p2.gety());
+        LineTo(p3);
+
+        LineTo(start);
+    }
+
+    public void drawTriangle(int sideLength) {
+        Point start = location.copy();
+        System.out.println("c-ordinates of Triangle:");
+
+        Point p1 = new Point(start.getx() + sideLength, start.gety());
+        LineTo(p1);
+
+        int height = (int) (Math.sqrt(3) / 2 * sideLength);
+        Point p2 = new Point(start.getx() + sideLength / 2, start.gety()); // this code drawing a line from Left corner
+                                                                           // to up/height.
+        // Point p2 = new Point(p1.getx() - sideLength / 2 , start.gety()); ---> this
+        // code drawing a line from right corner to up/height.
+        LineTo(p2);
+
+        LineTo(start);
+    }
+
+    public ArrayList<Point> getPath() {
+        return path;
+    }
+
+    public ArrayList<String> getpathHistory() {
+        return pathHistory;
+    }
+
+}
